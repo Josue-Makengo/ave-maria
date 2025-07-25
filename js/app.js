80d3d659768673bcca80520c09537ba71dd773af
Josue Makengo <josmakengim@gmail.com>
2025-07-25 15:46:38 +0100

    Ajout de plusieurs sections sur la page d'accueil (FAQ, Témoignages, Quick Order), finalisation du footer, intégration des liens vers les réseaux sociaux dans la navigation mobile, et création de la page page-en-construction pour présenter les pages et sections en cours de réalisation.

---
+++ js/app.js
@@ -0,0 +1,53 @@
+// pour la fermeture automatique du menu burger lorsque un lien est cliqué
+document.querySelectorAll('.navbar__link').forEach(link => {
+    link.addEventListener('click', () => {
+      document.getElementById('click').checked = false;
+    });
+  });
+
+// pour la section FAQ
+  document.querySelectorAll('.faq__question').forEach(button => {
+    button.addEventListener('click', () => {
+      const item = button.parentElement;
+      item.classList.toggle('open');
+    });
+  });
+
+//   pour le fonctionnement du popup commander 
+const orderBtn = document.getElementById('orderBtn');
+  const orderModal = document.getElementById('orderModal');
+  const cancelBtn = document.getElementById('cancelBtn');
+
+  orderBtn.addEventListener('click', () => {
+    orderModal.classList.add('show');
+  });
+
+  cancelBtn.addEventListener('click', () => {
+    orderModal.classList.remove('show');
+  });
+
+  // Optional: close on Esc key
+  document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+      orderModal.classList.remove('show');
+    }
+  });
+
+  // bouton flotante
+
+const scrollBtn = document.querySelector('.scroll-to-top');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 300) {
+    scrollBtn.classList.add('show');
+  } else {
+    scrollBtn.classList.remove('show');
+  }
+});
+
+scrollBtn.addEventListener('click', () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+});
